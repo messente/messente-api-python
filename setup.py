8d94b9f1e1fbd64c34f5db9e0bad64eb1d97c047
Erik Räni <erik.rani@messente.com>
2019-05-23 10:50:46 +0300

    1.0.2 (#2)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "messente-api"
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 # To install the library, run the following
 #
 # python setup.py install
@@ -29,12 +29,13 @@ setup(
     version=VERSION,
     description="Messente API",
     author_email="messente@messente.com",
-    url="https://pypi.org/project/messente-api",
+    url="https://github.com/messente/messente-api-python",
     keywords=["viber", "sms", "whatsapp", "phonebook"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
     long_description="""\
     [Messente](https://messente.com) is a global provider of messaging and user verification services. Use Messente API library to send and receive SMS, Viber and WhatsApp messages, blacklist phone numbers to make sure you&#39;re not sending any unwanted messages, manage contacts and groups.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.  # noqa: E501
-    """
+    """,
+    long_description_content_type="text/markdown"
 )
