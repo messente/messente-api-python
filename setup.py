525d3ba6c293b571ed2011c505ff959dbd87face
api-librarian <development@messente.com>
2019-10-21 06:09:17 +0000

    Travis update: Oct 2019 (Build 577)
    [skip ci]

--- setup.py
+++ setup.py
@@ -36,6 +36,5 @@ setup(
     include_package_data=True,
     long_description="""\
     [Messente](https://messente.com) is a global provider of messaging and user verification services.  * Send and receive SMS, Viber, WhatsApp and Telegram messages. * Manage contacts and groups. * Fetch detailed info about phone numbers. * Blacklist phone numbers to make sure you&#39;re not sending any unwanted messages.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.  # noqa: E501
-    """,
-    long_description_content_type="text/markdown"
+    """
 )
