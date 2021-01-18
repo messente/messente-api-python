f5c14195184b91b06a83d87291b0e5140df6dcc0
api-librarian <development@messente.com>
2021-01-18 17:19:32 +0000

    Travis update: Jan 2021 (Build 660)
    [skip ci]

--- messente_api/configuration.py
+++ messente_api/configuration.py
@@ -406,7 +406,7 @@ conf = messente_api.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.4.0\n"\
-               "SDK Package Version: 1.4.0".\
+               "SDK Package Version: 1.4.1".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
