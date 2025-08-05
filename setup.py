ad273899b4b8ab6a9d7a44c99bfb9cb52855a484
api-librarian <development@messente.com>
2025-08-05 08:19:06 +0000

    Travis update: Aug 2025 (Build 953)
    [skip ci]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 """
     Messente API
 
-    [Messente](https://messente.com) is a global provider of messaging and user verification services.  * Send and receive SMS, Viber and WhatsApp messages. * Manage contacts and groups. * Fetch detailed info about phone numbers. * Blacklist phone numbers to make sure you're not sending any unwanted messages.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.
+    [Messente](https://messente.com) is a global provider of messaging and user verification services.  * Send and receive SMS, Viber, WhatsApp and Telegram messages. * Manage contacts and groups. * Fetch detailed info about phone numbers. * Blacklist phone numbers to make sure you're not sending any unwanted messages.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.
 
     The version of the OpenAPI document: 2.0.0
     Contact: messente@messente.com
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "messente-api"
-VERSION = "2.5.2"
+VERSION = "2.5.0"
 PYTHON_REQUIRES = ">= 3.8"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 3.0.0",
@@ -38,14 +38,14 @@ setup(
     author="Messente",
     author_email="messente@messente.com",
     url="https://github.com/messente/messente-api-python",
-    keywords=["viber", "sms", "whatsapp", "phonebook"],
+    keywords=["viber", "sms", "telegram", "whatsapp", "phonebook"],
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
     license="Apache-2.0",
     long_description_content_type='text/markdown',
     long_description="""\
-    [Messente](https://messente.com) is a global provider of messaging and user verification services.  * Send and receive SMS, Viber and WhatsApp messages. * Manage contacts and groups. * Fetch detailed info about phone numbers. * Blacklist phone numbers to make sure you&#39;re not sending any unwanted messages.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.
+    [Messente](https://messente.com) is a global provider of messaging and user verification services.  * Send and receive SMS, Viber, WhatsApp and Telegram messages. * Manage contacts and groups. * Fetch detailed info about phone numbers. * Blacklist phone numbers to make sure you&#39;re not sending any unwanted messages.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.
     """,  # noqa: E501
     package_data={"messente_api": ["py.typed"]},
 )
